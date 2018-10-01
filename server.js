3015c991fd0da8beca950174ec87327008669b6e
unknown <stroms31@gmail.com>
2018-10-01 16:52:37 +0700

    cheange pgp uri

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var pgp = require('pg-promise')();
-var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
+var db = pgp('postgres://swvdjapflwlqyg:c0c2a74d8ea2d5012d6e41aadcf5242b7d3a52f6921650ac736b7a10885f5a91@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d1bhuue0uajvkm?ssl=true');
 
 var app = express();
 var bodyParser = require('body-parser');
