990412f1c3a9064a5e7a781fd8bf34bd7db1d32f
ipeeranan <stroms31@gmail.com>
2018-10-20 19:38:30 +0700

    test33

--- server.js
+++ server.js
@@ -64,19 +64,20 @@ app.get('/products/:pid', function (req, res) {
 });
 app.get('/users/:id', function (req, res) {
     var id = req.params.id;
-    var sql = 'select * from users';
-    if (id) {
-        sql += ' where id =' + id;
-    }
+    var sql = "select * from users where id=" + id;
+
     db.any(sql)
         .then(function (data) {
             console.log('DATA:' + data);
-            res.render('pages/users', { users: data })
+            res.render('pages/user_edit', { user: data[0] })
 
         })
         .catch(function (error) {
             console.log('ERROR:' + error);
+
         })
+
+
 });
 
 app.get('/users', function (req, res) {
